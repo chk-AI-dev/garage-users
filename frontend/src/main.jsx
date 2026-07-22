64a01199cfe4e525b3c4071a1e64c835acae687f
chk-AI-dev <chakradharmeshram@gmail.com>
2026-07-22 15:13:55 +0530

    main files update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// reactDom v18 uses createRoot instead of ReactDOM.render
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
